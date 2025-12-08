f2e74f12176a6d15a50d7aca70b50d73134525f1
Md. Habibur Rahman <habibcsepust@gmail.com>
2025-12-09 00:02:24 +0600

    ABVSSI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { UserComponent } from './user/user.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { StructuralDirectiveNgifVsIfComponent } from './structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component';
 import {StructuralDirectiveNgforVsForComponent} from './structural-directive-ngfor-vs-for/structural-directive-ngfor-vs-for.component';
+import { StructuralDirectiveNgswitchVsSwitchComponent } from "./structural-directive-ngswitch-vs-switch/structural-directive-ngswitch-vs-switch.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,UserComponent,DataBindingComponent,StructuralDirectiveNgifVsIfComponent,StructuralDirectiveNgforVsForComponent],
+  imports: [RouterOutlet, UserComponent, DataBindingComponent, StructuralDirectiveNgifVsIfComponent, StructuralDirectiveNgforVsForComponent, StructuralDirectiveNgswitchVsSwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/structural-directive-ngswitch-vs-switch/structural-directive-ngswitch-vs-switch.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+
+@Component({
+  selector: 'app-structural-directive-ngswitch-vs-switch',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './structural-directive-ngswitch-vs-switch.component.html',
+  styleUrl: './structural-directive-ngswitch-vs-switch.component.css'
+})
+export class StructuralDirectiveNgswitchVsSwitchComponent {
+
+}
